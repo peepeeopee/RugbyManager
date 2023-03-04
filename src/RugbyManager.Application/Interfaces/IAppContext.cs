84b76123152efc0728863c7a133bfa3f5de8d3c9
Peter Antoncich <PeterA@nvisionit.co.za>
2023-03-04 12:53:16 +0200

    added interface for database connection

---
+++ src/RugbyManager.Application/Interfaces/IAppContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RugbyManager.Domain.Entities;
+
+namespace RugbyManager.Application.Interfaces;
+
+public interface IAppContext
+{
+    DbSet<Player> Players { get; }
+    DbSet<Stadium> Stadiums { get; }
+    DbSet<Team> Teams { get; }
+    DbSet<Transfer> Transfers { get; }
+
+    Task SaveChangesAsync();
+}

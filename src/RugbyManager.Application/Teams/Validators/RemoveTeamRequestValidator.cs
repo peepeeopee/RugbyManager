3e588be9e935e954e6f3f903d9bf79c129a12fb2
Peter Antoncich <PeterA@nvisionit.co.za>
2023-03-05 19:27:13 +0200

    added tests to confirm remove team functionality

---
+++ src/RugbyManager.Application/Teams/Validators/RemoveTeamRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using RugbyManager.Application.Common.Models.Team;
+
+namespace RugbyManager.Application.Teams.Validators;
+
+public class RemoveTeamRequestValidator : AbstractValidator<RemoveTeamRequest>
+{
+    public RemoveTeamRequestValidator()
+    {
+        RuleFor(x => x.TeamId)
+            .GreaterThan(0);
+    }
+}
---
+++ test/RugbyManager.Application.UnitTests/TeamTests/RemoveTeamTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using RugbyManager.Application.Teams.Commands;
+using RugbyManager.Application.UnitTests.DataPersistence;
+using RugbyManager.Domain.Entities;
+using RugbyManager.Domain.Exceptions;
+
+namespace RugbyManager.Application.UnitTests.TeamTests;
+
+public class RemoveTeamTests : BaseTest
+{
+    [Fact]
+    public async Task RemoveTeam_GivenTeamWhichDoesntExist_TeamNotFoundExceptionThrown()
+    {
+        RemoveTeamCommand command = new()
+        {
+            TeamId = 21312
+        };
+
+        RemoveTeamCommandHandler handler = new(testDbContext);
+
+        var act =
+            handler.Awaiting(x => x.Handle(command, CancellationToken.None)
+            );
+
+        await act.Should()
+                 .ThrowAsync<TeamNotFoundException>();
+    }
+
+    [Fact]
+    public async Task RemoveTeam_GivenTeamExists_TeamRemoved()
+    {
+        Team team = new()
+        {
+            Name = "ToBeDeleted"
+        };
+
+        await testDbContext.Teams.AddAsync(team);
+
+        await testDbContext.SaveChangesAsync();
+
+        RemoveTeamCommand command = new()
+        {
+            TeamId = team.Id
+        };
+
+        RemoveTeamCommandHandler handler = new(testDbContext);
+
+        await handler.Handle(command, CancellationToken.None);
+    }
+}
